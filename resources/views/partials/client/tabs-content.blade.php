3e9c9a701192ac3f409433158970dce4e4a33900
Zulkifli Jufri <zulkiflijufri54@gmail.com>
2021-10-11 01:17:19 +0800

    component tabs detail campaign

---
+++ resources/views/partials/client/tabs-content.blade.php
@@ -0,0 +1,57 @@
+ <!-- Tab Contents -->
+ <div id="tab-contents" class="mt-5 px-0 sm:px-20 md:px-30 lg:px-0">
+     <div id="first">
+        {{ $campaign->stories }}
+     </div>
+     <div id="second">
+         <div class="flex p-2 md:pl-8 lg:p-3 mb-3">
+             <div class="flex-1 pl-1 pr-5">
+                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-coolGray-400" viewBox="0 0 20 20" fill="currentColor">
+                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                 </svg>
+             </div>
+             <div class="">
+                 <h1 class="text-coolGray-700 font-semibold text-lg">Dian</h1>
+                 <p class="mb-3 text-coolGray-500 font-medium text-sm">Rp. 300.000</p>
+                 <p class="text-coolGray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit cum
+                     ea laudantium? Eum, repellat.</p>
+             </div>
+         </div>
+         <div class="flex p-2 md:pl-8 lg:p-3 mb-3">
+             <div class="flex-1 pl-1 pr-5">
+                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-coolGray-400" viewBox="0 0 20 20" fill="currentColor">
+                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                 </svg>
+             </div>
+             <div class="">
+                 <h1 class="text-coolGray-700 font-semibold text-lg">Zulkifli</h1>
+                 <p class="mb-3 text-coolGray-500 font-medium text-sm">Rp. 500.000</p>
+                 <p class="text-coolGray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit cum
+                     ea laudantium? Eum, repellat.</p>
+             </div>
+         </div>
+         <div class="flex p-2 md:pl-8 lg:p-3 mb-3">
+             <div class="flex-1 pl-1 pr-5">
+                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-coolGray-400" viewBox="0 0 20 20" fill="currentColor">
+                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                 </svg>
+             </div>
+             <div class="">
+                 <h1 class="text-coolGray-700 font-semibold text-lg">Budi</h1>
+                 <p class="mb-3 text-coolGray-500 font-medium text-sm">Rp. 400.000</p>
+                 <p class="text-coolGray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit cum
+                     ea laudantium? Eum, repellat.</p>
+             </div>
+         </div>
+         <div class="text-center">
+             <a href="#" class="text-blue-400 hover:text-blue-500 hover:underline">Lebih banyak</a>
+         </div>
+     </div>
+     <div id="third">
+         Distribusi Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis accusamus
+         animi
+         ipsa possimus corrupti autem officiis mollitia neque veniam harum molestias excepturi
+         deleniti
+         consequuntur ex, fugit architecto quibusdam quod voluptatem.
+     </div>
+ </div>
---
+++ resources/views/partials/client/tabs-item.blade.php
@@ -0,0 +1,12 @@
+ <!-- Tabs -->
+ <ul id="tabs" class="flex justify-around w-full pt-2 text-center lg:space-x-40 lg:flex-row lg:justify-between">
+     <li class="px-4 py-2 -mb-px font-semibold text-gray-800 rounded-t opacity-50">
+         <a id="default-tab" href="#first" class="text-lg lg:text-md">Cerita</a>
+     </li>
+     <li class="px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50">
+         <a href="#second" class="text-lg lg:text-md">Donasi</a>
+     </li>
+     <li class="px-4 py-2 font-semibold text-gray-800 rounded-t opacity-50">
+         <a href="#third" class="text-lg lg:text-md">Penyaluran</a>
+     </li>
+ </ul>
