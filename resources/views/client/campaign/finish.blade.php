ba1367d199f94bc3a4dd58622b41c0663384a59a
Zulkifli Jufri <zulkiflijufri54@gmail.com>
2021-10-12 23:45:29 +0800

    add url finish

--- resources/views/client/campaign/finish.blade.php
+++ resources/views/client/campaign/finish.blade.php
@@ -9,7 +9,7 @@
             <img src="/img/finish.png" alt="" class="w-96 h-96">
             <p class="text-2xl font-semibold md:text-3xl mb-2">Terima Kasih</p>
             <p class="mb-8 text-coolGray-600">Donasi kamu akan segera kami proses</p>
-            <a href="#" class="px-5 py-3 font-semibold rounded bg-blue-500 text-coolGray-50 hover:bg-blue-600 hover:text-coolGray-100 transition duration-300">Kembali ke halaman utama</a>
+            <a href="/" class="px-5 py-3 font-semibold rounded bg-blue-500 text-coolGray-50 hover:bg-blue-600 hover:text-coolGray-100 transition duration-300">Kembali ke halaman utama</a>
         </div>
     </div>
 </section>
