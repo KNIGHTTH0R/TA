1493731d9cdf5398342338cbde5f7e1560fbc8ef
Zulkifli Jufri <zulkiflijufri54@gmail.com>
2021-10-12 20:46:46 +0800

    modified form donate

--- resources/views/client/campaign/form.blade.php
+++ resources/views/client/campaign/form.blade.php
@@ -13,7 +13,7 @@
         <div class="lg:px-70 xl:px-80">
             <div class="">
                 <section class="bg-coolGray-100 text-coolGray-900">
-                    <form action="{{ route('donasi.form', $campaign) }}" method="post" class="container flex flex-col">
+                    <form action="{{ route('donasi.form', $campaign) }}" method="post" class="container flex flex-col" id="form-donate">
                         @csrf
                         {{-- <input type="hidden" name="campaign_id" value="{{ $campaign->id }}"> --}}
                         <fieldset class="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-coolGray-50">
@@ -21,12 +21,8 @@
                                 <div class="col-span-full space-y-3">
                                     <label for="amount" class="text-sm">Jumlah</label>
                                     <input id="amount" type="number" name="amount" class="w-full p-3 rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-600 focus:outline-none border-coolGray-300 text-coolGray-900" autocomplete="off" value="{{ old('amount') }}">
-                                    @error('amount')
-                                        <span class="text-red-400 text-sm">{{ $message }}</span>
-                                    @enderror
-                                    <div class="flex items-center space-x-1">
-                                        <input type="checkbox" name="anonim" id="checkbox" {{ 'checked' ? 'value=true': 'value=false' }}><label for="checkbox" class="text-xs text-coolGray-400">Jadikan Hamba Allah</label>
-                                    </div>
+                                    <span class="text-red-400 text-sm hidden" id="err-amount"></span>
+                                    <span class="text-coolGray-400 text-sm" id="info-amount">minimal puluhan ribu</span>
                                 </div>
                                 <div class="col-span-full">
                                     <label for="email" class="text-sm">Pembayaran</label>
@@ -38,25 +34,23 @@
                                                 <img src="/img/gopay.png" alt="" class="w-full h-full object-contain">
                                             </label>
                                             <label id="pay-item">
-                                                <input type="radio" id="pay" value="mandiri">
-                                                <img src="/img/mandiri.png" alt="" class="w-full h-full object-contain">
-                                            </label>
-                                            <label id="pay-item">
                                                 <input type="radio" id="pay" value="bni">
                                                 <img src="/img/bni.png" alt="" class="w-full h-full object-contain">
                                             </label>
                                         </div>
-                                        @error('pay')
-                                        <span class="text-red-400 text-sm">{{ $message }}</span>
-                                        @enderror
+                                        <span class="text-red-400 text-sm hidden" id="err-pay"></span>
                                     </div>
                                 </div>
                                 <div class="col-span-full space-y-3">
                                     <label for="bio" class="text-sm">Doa</label>
                                     <textarea id="bio" name="pray" class="w-full p-3 rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-600 focus:outline-none border-coolGray-300 text-coolGray-900">{{ old('pray') }}</textarea>
+
+                                    <div class="flex items-center space-x-1">
+                                            <input type="checkbox" name="anonim" id="checkbox" {{ 'checked' ? 'value=true': 'value=false' }}><label for="checkbox" class="text-xs text-coolGray-400">Jadikan Hamba Allah</label>
+                                    </div>
                                 </div>
                                 <div class="col-span-full space-y-3">
-                                    <button type="submit" class="w-full bg-coolGray-200 hover:bg-blue-500 py-3 px-2 text-coolGray-500 hover:text-blue-100 rounded-sm transition duration-500 ease-out">Donasi</button>
+                                    <button type="button" class="w-full bg-coolGray-200 hover:bg-blue-500 py-3 px-2 text-coolGray-500 hover:text-blue-100 rounded-sm transition duration-500 ease-out" id="btn-donate">Donasi</button>
                                 </div>
                             </div>
                         </fieldset>
@@ -100,6 +94,55 @@
             }
         })
     }
+
+    $("#btn-donate").click(function () {
+        let url = $("#form-donate").attr('action')
+        let method = $("#form-donate").attr('method')
+        let form_donate = $("form").serialize()
+
+        let infoAmount = $("#info-amount")
+        let errAmount = $("#err-amount")
+        let errPay = $("#err-pay")
+
+        //errAmount.addClass('hidden', true)
+        errPay.addClass('hidden', true)
+
+        $.ajax({
+            method: method,
+            url: url,
+            data: form_donate,
+            success: function (res) {
+                snap.pay(res.token, {
+                    // Optional
+                    onSuccess: function (result) {
+                        //
+                    },
+                    // Optional
+                    onPending: function (result) {
+                        //
+                    },
+                    // Optional
+                    onError: function (result) {
+                        //
+                    }
+                });
+            },
+            error: function (err) {
+                let errors = err.responseJSON.errors;
+
+                if (errors.amount) {
+                    infoAmount.addClass('hidden', true)
+                    errAmount.removeClass('hidden')
+                    errAmount.text(errors.amount[0])
+                }
+
+                if (errors.pay) {
+                    errPay.removeClass('hidden')
+                    errPay.text(errors.pay[0])
+                }
+            }
+        })
+    })
 
 </script>
 @endpush
