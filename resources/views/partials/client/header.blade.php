6493db943366cac009ed6ee3291a1b8fb8791097
Zulkifli Jufri <zulkiflijufri54@gmail.com>
2021-10-12 21:43:33 +0800

    test edit

--- resources/views/partials/client/header.blade.php
+++ resources/views/partials/client/header.blade.php
@@ -2,7 +2,7 @@
      <div class="container flex flex-col justify-center p-8 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
          <div class="flex flex-col justify-center py-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
              <h1 class="text-5xl font-bold leading-none sm:text-6xl">Platform<span class="text-sky-600"> teman bantu
-                     teman</span></h1>
+                     teman Edit</span></h1>
              <p class="mt-6 mb-8 text-lg sm:mb-12">Buat teman kamu kembali tersenyum dengan bantuan <br class="hidden md:inline"> yang kamu berikan</p>
              <div class="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
                  <a href="#" class="px-8 py-3 text-lg font-semibold rounded border bg-blue-400 hover:bg-sky-600 transition duration-500 text-coolGray-100 hover:text-coolGray-50 text-center tracking-wide">MULAI</a>
