65e900065b2bfcc756f6ab61218e71608d367e17
Zulkifli Jufri <zulkiflijufri54@gmail.com>
2021-10-21 22:48:14 +0800

    component status donate in dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -13,22 +13,11 @@
             @include('partials.client.sidebar')
 
             <div class="flex flex-1 px-32 pt-5">
-                <!-- <section class="p-6 bg-coolGray-100 text-coolGray-800"> -->
                 <div class="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-3">
-                    <div class="flex flex-col justify-start m-2 lg:m-6 text-green-500">
-                        <p class="text-4xl font-bold leading-none lg:text-5xl">{{ count($transaction_success) ?? 0 }}</p>
-                        <p class="text-sm sm:text-base">Donasi Success</p>
-                    </div>
-                    <div class="flex flex-col justify-start m-2 lg:m-6 text-yellow-500">
-                        <p class="text-4xl font-bold leading-none lg:text-5xl">{{ count($transaction_pending) ?? 0 }}</p>
-                        <p class="text-sm sm:text-base">Donasi Pending</p>
-                    </div>
-                    <div class="flex flex-col justify-start m-2 lg:m-6 text-red-500">
-                        <p class="text-4xl font-bold leading-none lg:text-5xl">{{ count($transaction_expired) ?? 0 }}</p>
-                        <p class="text-sm sm:text-base">Donasi Expired</p>
-                    </div>
+                   <x-admin.dashboard title="Donasi Success" type="green" total="{{ count($transaction_success) ?? 0 }}"/>
+                   <x-admin.dashboard title="Donasi Pending" type="yellow" total="{{ count($transaction_pending) ?? 0 }}"/>
+                   <x-admin.dashboard title="Donasi Expired" type="red" total="{{ count($transaction_expired) ?? 0 }}"/>
                 </div>
-                <!-- </section> -->
             </div>
 
         </div>
---
+++ resources/views/components/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+ <div class="flex flex-col justify-start m-2 lg:m-6 text-{{ $type }}-500">
+     <p class="text-4xl font-bold leading-none lg:text-5xl">{{ $total }}</p>
+     <p class="text-sm sm:text-base">{{ $title }}</p>
+ </div>
