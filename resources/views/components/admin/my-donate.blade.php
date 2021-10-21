a435b458a7506a5a8dc4701976535cd9e1fb3b01
Zulkifli Jufri <zulkiflijufri54@gmail.com>
2021-10-22 07:07:11 +0800

    add category for donate user

--- resources/views/components/admin/my-donate.blade.php
+++ resources/views/components/admin/my-donate.blade.php
@@ -4,6 +4,7 @@
          <div class="flex bg-white rounded-md shadow p-2 max-w-md">
              <img alt="" class="object-cover w-32 h-32 bg-coolGray-500 mr-5 rounded" src="https://source.unsplash.com/128x128/?work">
              <div class="flex flex-col w-full p-2">
+                <span class="text-sm tracking-wider text-sky-600 mb-1">{{ $transaction->campaign->category->title }}</span>
                  <span class="text-md font-semibold text-sky-700">{{ $transaction->campaign->title }}</span>
                  <span class="text-sm font-semibold mt-2">Rp. {{ $transaction->amount }}</span>
                  <input type="hidden" name="snap_token" value="{{ $transaction->snap_token }}" id="snap_token">
